c0fb1725f73a29580cea81d5b5f97ef6adb516c5
Anton Tokarev <anton999@bk.ru>
2023-05-11 00:14:53 +0500

    Start build

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="no-underline hover:underline cursor-pointer fw-700">Привет Мирок</h1>
+      </header>
+    </div>
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  entry: "./public/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /.css$/,
+        use: [isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
